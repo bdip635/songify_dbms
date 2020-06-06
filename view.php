668c983ec4cec18f7e4b2c97efb2e202c1f95866
Dipanshu Barnwal <58032366+bdip635@users.noreply.github.com>
2020-06-06 21:38:04 +0530

    Update view.php

--- view.php
+++ view.php
@@ -58,6 +58,74 @@ else
 
 	$row = $stmt->fetch(PDO::FETCH_OBJ);
 }
+if(isset($_POST['language']))
+{
+    $apply1=$_POST['language'];
+    $songs=[];
+    $all_songs = $pdo->query("SELECT songs.Song_id,songs.Name,songs.Release_Date,songs.Rating,
+    artist.Name AS Artist,album.NAME AS Album, genre.Name AS Genre 
+    FROM songs JOIN album JOIN genre JOIN artist JOIN language
+    ON songs.Album_id=album.ALBUM_id AND songs.genre_id=genre.Genre_id 
+    AND songs.Artist_id=artist.Artist_id AND language.Language_id=songs.Lang_id
+    WHERE songs.Lang_id=$apply1");
+}
+else if(isset($_POST['genre']))
+{
+    $apply3=$_POST['genre'];
+    $songs=[];
+    $all_songs = $pdo->query("SELECT songs.Song_id,songs.Name,songs.Release_Date,songs.Rating,
+    artist.Name AS Artist,album.NAME AS Album, genre.Name AS Genre 
+    FROM songs JOIN album JOIN genre JOIN artist
+    ON songs.Album_id=album.ALBUM_id AND songs.Genre_id=genre.Genre_id 
+    AND songs.Artist_id=artist.Artist_id
+    WHERE songs.Genre_id=$apply3");
+}
+else if(isset($_POST['mood']))
+{
+    $apply2=$_POST['mood'];
+    $songs=[];
+    $all_songs = $pdo->query("SELECT songs.Song_id,songs.Name,songs.Release_Date,songs.Rating,
+    artist.Name AS Artist,album.NAME AS Album, genre.Name AS Genre 
+    FROM songs JOIN album JOIN genre JOIN artist JOIN mood
+    ON songs.Album_id=album.ALBUM_id AND songs.genre_id=genre.Genre_id 
+    AND songs.Artist_id=artist.Artist_id
+    WHERE songs.Mood_id=$apply2");
+}
+else
+{
+    $songs=[];
+    $all_songs = $pdo->query("SELECT songs.Song_id,songs.Name,songs.Release_Date,songs.Rating,
+    artist.Name AS Artist,album.NAME AS Album, genre.Name AS Genre 
+    FROM songs JOIN album JOIN genre JOIN artist 
+    ON songs.Album_id=album.ALBUM_id AND songs.genre_id=genre.Genre_id AND songs.Artist_id=artist.Artist_id");
+}
+while ( $row2 = $all_songs->fetch(PDO::FETCH_OBJ) ) 
+{
+    $songs[] = $row2;
+}
+
+$lang=[];
+$all_lang = $pdo->query("SELECT * FROM language");
+
+while ( $row3 = $all_lang->fetch(PDO::FETCH_OBJ) ) 
+{
+    $lang[] = $row3;
+}
+$mood=[];
+$all_mood = $pdo->query("SELECT * FROM mood");
+
+while ( $row4 = $all_mood->fetch(PDO::FETCH_OBJ) ) 
+{
+    $mood[] = $row4;
+}
+$genre=[];
+$all_genre = $pdo->query("SELECT * FROM genre");
+
+while ( $row5 = $all_genre->fetch(PDO::FETCH_OBJ) ) 
+{
+    $genre[] = $row5;
+}
+
 ?>
 
 
@@ -86,7 +154,7 @@ if ( $status !== false )
 </p>
 <h1>Welcome <?php echo htmlentities($row->UserName); ?></h1>
 
-<a href="add.php">Add Songs</a> |
+<a href="fav.php">Favourite Songs</a> |
 <a href="contact.php">Contact</a> |
 <a href="logout.php">Logout</a>
         <hr>
@@ -97,50 +165,66 @@ if ( $status !== false )
         <div id="fav">
         <button>Favourite</button>
         </div>
-        <form method="POST"> 
+        
             <!-- action="song.html"> -->
         <div class="box">
         <div class="lan extra">
             <h4>Language</h4>
             <hr>
-            <input type="radio" name="language" value="English">English<br>
-            <input type="radio" name="language" value="Hindi">Hindi<br>
+            <form method="POST"> 
+            <?php foreach($lang as $langa) : ?>
+            <input type="radio" name="language" value=<?php echo $langa->Language_id;?>> <?php echo $langa->Name;?> 
+            <?php endforeach; ?>
+            <input type="submit" name="button" value="Apply"/></form>
         </div>
         <div class="mood extra">
             <h4>Mood</h4>
             <hr>
-            <input type="radio" name="mood" value="mood1">Mood1<br>
-            <input type="radio" name="mood" value="mood2">Mood2<br>
-        </div>
-        <div class="album extra">
-            <h4>Album</h4>
-            <hr>
-            <input type="radio" name="alb" value="album1">Album1<br>
-            <input type="radio" name="alb" value="album2">Album2<br>
+            <form method="POST"> 
+            <?php foreach($mood as $mooda) : ?>
+            <input type="radio" name="mood" value=<?php echo $mooda->Mood_id;?>> <?php echo $mooda->Name;?> 
+            <?php endforeach; ?>
+            <input type="submit" name="button" value="Apply"/></form>
         </div>
         <div class="genre extra">
             <h4>Genre</h4>
             <hr>
-            <input type="radio" name="gen" value="genre1">Genre1<br>
-            <input type="radio" name="gen" value="genre2">Genre2<br>
+            <form method="POST">
+            <?php foreach($genre as $genrea) : ?>
+            <input type="radio" name="genre" value=<?php echo $genrea->Genre_id;?>> <?php echo $genrea->Name;?> 
+            <?php endforeach; ?>
+            <input type="submit" name="button" value="Apply"/></form>
         </div>
         </div>
     </form>
 
 <h2>Songs</h2>
 <!-- Inserting tables -->
-
-<?php if(empty($autos)): 
+<?php if(empty($songs)): 
     echo "None"; 
 
  else: ?>
-<ul>
-<?php foreach($autos as $auto) : ?>
-    <li>
-    <?php echo $auto->make; ?> <?php echo $auto->year; ?> <?php echo $auto->mileage; ?> 
-    </li>
+<table style="width:70%">
+    <tr>
+    <th><?php echo "Name"; ?> </th>
+    <th><?php echo "Artist"; ?> </th>
+    <th><?php echo "Album"; ?> </th>
+    <th><?php echo "Rating"; ?> </th>
+    <th><?php echo "Release_Date"; ?> </th>
+    <th><?php echo "Genre";?> </th>
+    </tr>
+<?php foreach($songs as $song) : ?>
+    <tr>
+    <td><?php echo $song->Name; ?> </td>
+    <td><?php echo $song->Artist; ?> </td>
+    <td><?php echo $song->Album; ?> </td>
+    <td><?php echo $song->Rating; ?> </td>
+    <td><?php echo $song->Release_Date; ?> </td>
+    <td><?php echo $song->Genre;?> </td>
+    <td><a href="fav.php?song_id=<?php echo $song->Song_id; ?>">Add to Fav.</a>
+    </tr>
 <?php endforeach; ?>
-</ul>
+</table>
 <?php endif;?>
 
 </div>
